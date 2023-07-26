299660b9e406e37608e5a4768ece3b55a8436bf0
supriya040 <supriyapushpa2000@gmail.com>
2023-07-26 15:34:01 +0530

    hey

--- server.py
+++ server.py
@@ -2,5 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def root():
-   return "Hello Supriya"
+   return "Hello"
 app.run(host="0.0.0.0",port=4010, debug=True)
